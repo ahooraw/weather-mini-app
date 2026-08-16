304203cdac11d56bb6a0ba05a96065394281e4f1
unknown <mahdiikeshvarii@gmail.com>
2026-08-16 22:27:17 +0330

    Add weather error handling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "YAK";
 
 const searchForm = document.querySelector(".search-form");
 const cityInput = document.querySelector("#city-input");
@@ -22,8 +22,46 @@ async function getWeather(city) {
         const response = await fetch(API_URL);
         const data = await response.json();
 
-        console.log(data);
+        if (data.cod !== 200) {
+            showError(data.message);
+            return;
+        }
+
+         displayWeather(data);
     } catch (error) {
         console.error("Error:", error);
     }
+}
+
+function displayWeather(data) {
+
+    const cityName = document.querySelector("#city-name");
+    const description = document.querySelector("#weather-description");
+    const temperature = document.querySelector("#temperature");
+    const humidity = document.querySelector("#humidity");
+    const windSpeed = document.querySelector("#wind-speed");
+    const feelsLike = document.querySelector("#feels-like");
+
+
+    cityName.textContent = data.name;
+
+    description.textContent = data.weather[0].description;
+
+    temperature.textContent = Math.round(data.main.temp);
+
+    humidity.textContent = `${data.main.humidity}%`;
+
+    windSpeed.textContent = `${data.wind.speed} m/s`;
+
+    feelsLike.textContent = `${Math.round(data.main.feels_like)}°C`;
+}
+
+function showError(message) {
+
+    const cityName = document.querySelector("#city-name");
+    const description = document.querySelector("#weather-description");
+
+    cityName.textContent = "Error";
+
+    description.textContent = message;
 }
